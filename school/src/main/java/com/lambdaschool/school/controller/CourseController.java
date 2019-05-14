4bcded656c06494fa0c30bb3fcefab2d3b35310e
Austin Powell <Austin.PowellFSW@gmail.com>
2019-05-14 14:58:53 -0400

    Completed MVP

--- school/src/main/java/com/lambdaschool/school/controller/CourseController.java
+++ school/src/main/java/com/lambdaschool/school/controller/CourseController.java
@@ -1,8 +1,11 @@
 package com.lambdaschool.school.controller;
 
 import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.ErrorDetail;
+import com.lambdaschool.school.model.Student;
 import com.lambdaschool.school.service.CourseService;
 import com.lambdaschool.school.view.CountStudentsInCourses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +19,16 @@ public class CourseController
 {
     @Autowired
     private CourseService courseService;
-
+    @ApiOperation(value = "Return all courses", response = Course.class, responseContainer = "List")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "integr", paramType = "query",
+                    value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is ascending. " +
+                            "Multiple sort criteria are supported.")})
     @GetMapping(value = "/courses", produces = {"application/json"})
     public ResponseEntity<?> listAllCourses()
     {
@@ -29,7 +41,12 @@ public class CourseController
     {
         return new ResponseEntity<>(courseService.getCountStudentsInCourse(), HttpStatus.OK);
     }
-
+    @ApiOperation(value = "Deletes a Course",
+            response = void.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Course Deleted Successfully", response = void.class),
+            @ApiResponse(code = 500, message = "Error Deleting Course", response = ErrorDetail.class)
+    } )
     @DeleteMapping("/courses/{courseid}")
     public ResponseEntity<?> deleteCourseById(@PathVariable long courseid)
     {
